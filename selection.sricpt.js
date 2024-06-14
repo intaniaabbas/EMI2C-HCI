148e54c0a0a480bf8e88ab9bb918ff787aa88621
intaniaabbas <khoerunnisa.abbas@binus.ac.id>
2024-06-14 21:21:40 +0700

    Add files via upload

--- selection.sricpt.js
+++ selection.sricpt.js
@@ -1,14 +1,16 @@
-document.getElementById('public-transport').addEventListener('click', function() {
-    alert('Public Transportation selected!');
-});
-
-document.getElementById('private-transport').addEventListener('click', function() {
-    alert('Private Transportation selected!');
-    
-});
-
 // Toggle dropdown menu
 document.getElementById('menuIcon').addEventListener('click', function() {
     let dropdownMenu = document.getElementById('dropdownMenu');
     dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('public-transport').addEventListener('click', function() {
+        window.location.href = 'public-transport.html';
+    });
+
+    document.getElementById('private-transport').addEventListener('click', function() {
+        window.location.href = 'private-transport.html';
+    });
+});
+
